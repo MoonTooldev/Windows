93c8b5703ace49209d65d4ef81ea4419b6ecd6a2
MoonTool <mayonezuch@gmail.com>
2025-05-09 18:56:54 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,44 +1,41 @@
+let ala = new Audio('ala.mp3');
+ala.loop = true;
+
 function play_se(){
-	var ala = new Audio('ala.mp3');
-	ala.loop = true;
 	ala.play();
 	navigator.vibrate([200, 100, 200, 100, 200, 100, 200]);
 }
 
+function goFullScreen() {
+	let elem = document.documentElement;
+	if (elem.requestFullscreen) {
+		elem.requestFullscreen();
+	} else if (elem.webkitRequestFullscreen) {
+		elem.webkitRequestFullscreen();
+	} else if (elem.msRequestFullscreen) {
+		elem.msRequestFullscreen();
+	}
+}
+
 $(function(){
-
-
-	
 	$('.modal').modal({dismissible: false});
 	$('#alert').modal('open');
+
 	$('#close').click(function(){
 		$('#alert').modal('close');
-		play_se();
+		goFullScreen();  // ユーザー操作中！
+		play_se();        // 音も再生！
 	});
-	
+
 	var device = navigator.userAgent.match(/Android|iPhone|iPad|Windows|Mac/);
-	
 	if (device == null){
 		device = '端末';
 	}
 	$('#device').text(device);
-	
+
 	var time = 200;
 	setInterval(function(){
 		time--;
 		$('#timer').text(time);
 	}, 1000);
-	
 });
-
-function goFullScreen() {
-	let elem = document.documentElement; // ページ全体（<html>要素）
-
-	if (elem.requestFullscreen) {
-		elem.requestFullscreen();
-	} else if (elem.webkitRequestFullscreen) { // Safari用
-		elem.webkitRequestFullscreen();
-	} else if (elem.msRequestFullscreen) { // IE用
-		elem.msRequestFullscreen();
-	}
-}
